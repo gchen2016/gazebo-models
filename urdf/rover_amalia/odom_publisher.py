d4cb821ef9d455bbb47e7860d8eb753049c4ee5f
clynamen <clynamen@gmail.com>
2014-12-08 01:01:11 +0100

    add odom_publisher.py

---
+++ urdf/rover_amalia/odom_publisher.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import rospy
+import sys
+
+import tf
+from nav_msgs.msg import Odometry
+
+class OdomPublisher():
+    def __init__(self, from_tf_name, to_tf_name, odom_topic):
+        rospy.Subscriber(odom_topic, Odometry, self.on_odom_msg)
+        self.tf_broadcaster = tf.TransformBroadcaster()
+
+    def on_odom_msg(self, msg):
+        pose = msg.pose.pose.position
+        self.tf_broadcaster.sendTransform( (pose.x, pose.y, 0),
+                        (0, 0, 0, 1), 
+                        #rospy.Time(secs=msg.stamp.secs, nsecs=msg.stamp.nsecs)
+                        msg.header.stamp,
+                        to_tf_name,
+                        from_tf_name)
+        
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print ("Usage: " + sys.argv[0] + " from_tf_name to_tf_name /odom_topic ")
+        sys.exit(-1)
+    rospy.init_node("odom_tf_publisher")
+    from_tf_name = sys.argv[1]
+    to_tf_name = sys.argv[2]
+    odom_topic = sys.argv[3]
+    odom_publisher = OdomPublisher(from_tf_name, to_tf_name, odom_topic)
+    rospy.spin()
